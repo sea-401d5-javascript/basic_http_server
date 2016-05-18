7ce5d750047e5bc62cc9d745ee00307f45570330
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-05-18 10:21:52 -0700

    completed the greet_name task, going to write test

--- drew/http_server.js
+++ drew/http_server.js
@@ -6,8 +6,27 @@ http.createServer((req, res) => {
   if (req.url === '/time' && req.method === 'GET') {
     let currentTime = new Date().getTime();
     let currentTimeToString = currentTime.toString();
-    // console.log(currentTimeToString);
     res.write(currentTimeToString);
     return res.end();
   }
+
+//greet/name in url gives back name in string
+//  -need to find out how to write url
+//  -need to make it so any part after      //   greet is what the string is
+
+let greetName = req.url.substring(7);
+
+  if(req.url === ('/greet/' + greetName) && req.method === 'GET') {
+    let greetNameToString = greetName.toString();
+    res.write(greetNameToString);
+    return res.end();
+  }
+
+  res.writeHead(404, {
+    'Content-Type': 'text/html'
+  });
+  res.write('NOT FOUND')
+console.log(req.url);
+  res.end();
+
 }).listen(3000);
