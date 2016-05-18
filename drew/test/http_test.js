525a3fae0bc75e67dd604fdb7011408181f00a68
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2016-05-18 09:52:55 -0700

    got a test for the time but doesn't pass

--- drew/test/http_test.js
+++ drew/test/http_test.js
@@ -1,5 +1,24 @@
+'use strict';
+
 const chai = require('chai');
 const chaiHTTP = require('chai-http');
 const expect = chai.expect;
 chai.use(chaiHTTP);
 const request = chai.request;
+
+require('../http_server');
+
+describe('HTTP tests', () => {
+  it('should provide a time', (done) => {
+    let currentTime = new Date().getTime();
+    let currentTimeToString = currentTime.toString();
+    request('localhost:3000')
+    .get('/time')
+    .end((err, res) => {
+      expect(err).to.eql(null);
+      expect(res).to.have.status(200);
+      expect(res.text).to.eql(currentTimeToString);
+      done();
+    })
+  })
+})
