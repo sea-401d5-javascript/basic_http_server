e7160b15a1f6001c4db0460c4c108430bbf38c3a
simonszc <zsimonson26@gmail.com>
2016-05-18 12:32:27 -0700

    server works, gulpfile/package.json present, first two tests work. struggling with post test

---
+++ zach/server.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const http = require('http');
+const users = [];
+
+http.createServer((req, res) => {
+  if(req.url === '/time' && req.method === 'GET'){
+    let newDate = new Date();
+    module.exports.newDate = newDate.getUTCMinutes();
+    res.write(newDate.toString());
+    res.end();
+    return;
+  }
+  if(req.url.startsWith('/greet/') && req.method === 'GET') {
+    let name = res.greetName = req.url.split('/')[2];
+    let greeting = res.text = 'Hello there, ' + name + '.\n';
+    res.write(greeting);
+    res.end();
+    return;
+  }
+  if(req.url.startsWith('/greet') && req.method === 'POST') {
+    req.on('data', function(chunk) {
+      let newUser = JSON.parse(chunk.toString()).newUser;
+      res.write("Hello there, " + newUser + ".\n");
+      users.push(newUser);
+      res.end();
+      return;
+    });
+  }
+  else {
+    res.write('NOT FOUND');
+    res.writeHead(404, {
+      'Content-Type': 'text/html'
+    });
+    res.end();
+  }
+}).listen(3000);
---
+++ zach/test/test.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const chai = require('chai');
+const expect = require('chai').expect;
+const chaiHTTP = require('chai-http');
+chai.use(chaiHTTP);
+const server = require(__dirname + '/../server.js');
+
+describe('http server', () => {
+  it('should return the time when we enter /time into the url', (done) => {
+    let testDate = new Date();
+    chai.request('http://localhost:3000')
+      .get('/time')
+      .end((err, res) => {
+        expect(server.newDate).to.eql(testDate.getUTCMinutes());
+        done();
+      })
+  })
+  it('should return a greeting to the name we entered with /greet/name', (done) => {
+    chai.request('http://localhost:3000')
+      .get('/greet/CRAZYEYES')
+      .end((err, res) => {
+        expect(res.text).to.eql('Hello there, CRAZYEYES.\n');
+        done();
+      })
+  })
+  // it('should return a greeting to the name we entred with /greet in JSON format', (done) => {
+  //   chai.request('http://localhost:3000')
+  //     .post('/greet')
+  //     .field('newUser', 'Zach')
+  //     .end((err, res) => {
+  //       expect(res.text).to.eql('Hello there, Zach.\n');
+  //       done();
+  //     })
+  // })
+})
