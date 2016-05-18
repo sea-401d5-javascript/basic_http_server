0da7a89e5add226959503fc0cb86d84145617ec8
Victor Benavente <VMB@Victors-MacBook-Air.local>
2016-05-18 13:25:27 -0700

    fixed testing to actually test for what was posting

--- vic-class07-assignment/test/http_test.js
+++ vic-class07-assignment/test/http_test.js
@@ -40,10 +40,11 @@ describe('HTTP test', () => {
   it('should give me what was posted', () => {
     request('localhost:3000/greet')
     .post('/greet')
+    .send('{"name": "vic"}')
     .end((err, res) => {
       expect(err).to.eql(null);
       expect(res).to.have.status(200);
-      expect(json.parse('{"name": "vic"}').name).to.eql('vic')
+      expect(json.parse(res.body.name)).to.eql('vic')
     })
   })
 });
