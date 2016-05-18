c8e0913c6d3c9364dc8d96d25c2898a1a43a41dd
Chris Perez <chris.k.perez@gmail.com>
2016-05-18 12:29:12 -0700

    http assignment pretty much done

---
+++ server.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const http = require('http');
+
+http.createServer((req, res)=>{
+  var paramArray = req.url.split('/');
+
+  if (paramArray[1] === 'time'){
+    console.log(paramArray)
+    res.write(new Date().toString() + '\n');
+    return res.end();
+  };
+
+  if (paramArray[1] === 'greet' && paramArray[2]){
+    console.log(paramArray);
+    res.write(`Wuddup, ${paramArray[2]}\n`);
+    return res.end();
+  }
+
+  if (paramArray[1] === 'greet' && req.method === 'POST'){
+    let name;
+    req.on('data', (data)=>{
+      name = JSON.parse(data).name;
+    })
+    req.on('end', ()=>{
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      console.log(name);
+      return res.end();
+    })
+    res.write('neat.\n');
+  }
+
+  res.writeHead(404, {
+    'Content-Type': 'text/html'
+  })
+  res.write('NOT FOUND\n');
+  res.end();
+
+}).listen(3000);
---
+++ test/test.js
@@ -0,0 +1,46 @@
+const chai = require('chai');
+const chaiHTTP = require('chai-http');
+const expect = chai.expect;
+chai.use(chaiHTTP);
+const request = chai.request;
+require(__dirname + '/../server.js');
+
+describe('HTTP tests', ()=>{
+  it('should greet on get greet', (done)=>{
+    request('localhost:3000')
+      .get('/greet/me')
+      .end((err, res)=>{
+        expect(err).to.eql(null);
+        expect(res).to.have.status(200);
+        expect(res.text).to.eql('Wuddup, me\n');
+        done();
+      })
+  })
+  it('should catch not found', (done)=>{
+    request('localhost:3000')
+      .get('/notthere')
+      .end((err, res)=>{
+        expect(res).to.have.status(404);
+        expect(res.text).to.eql('NOT FOUND\n');
+        done();
+      })
+  })
+  it('should give me the current date/time', (done)=>{
+    request('localhost:3000')
+      .get('/time')
+      .end((err, res)=>{
+        expect(err).to.eql(null);
+        expect(res.text).to.eql(new Date().toString() + '\n')
+        done();
+      })
+  })
+  it('should grab the name from a json object', (done)=>{
+    request('localhost:3000')
+      .post('/greet')
+      .end((err, res)=>{
+        expect(err).to.eql(null);
+        expect(res.text).to.eql('neat.\n')
+        done();
+      })
+  })
+})
